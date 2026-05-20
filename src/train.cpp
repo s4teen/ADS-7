63da3c9d0dd0cf52c75d8fb492bda095f040bfb7
s4teen <112770218+s4teen@users.noreply.github.com>
2026-05-20 20:52:36 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,63 +1,52 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : countOp(0), first(nullptr) {}
-
-void Train::addCar(bool light) {
-    Car* newCar = new Car{light, nullptr, nullptr};
-    if (!first) {
-        first = newCar;
-        newCar->next = newCar;
-        newCar->prev = newCar;
-    } else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
-}
-
-int Train::getLength() {
-    countOp = 0;
-    if (!first) return 0;
-
-    bool initialState = first->light;
-    first->light = !initialState;
-    countOp++;
-
-    int steps = 1;
-    Car* current = first;
-
-    while (true) {
-        current = current->next;
-        countOp++;
-        steps++;
-
-        if (current->light == !initialState) {
-            current->light = initialState;
-            countOp++;
-
-            for (int i = 0; i < steps; i++) {
-                current = current->prev;
-                countOp++;
-            }
-
-            if (current->light == initialState) {
-                current->light = !initialState;
-                countOp++;
-                steps = 1;
-            } else {
-                current->light = !initialState;
-                countOp++;
-                break;
-            }
-        }
-    }
-
-    return steps;
-}
+Train::Train() : first(nullptr), countOp(0) {}
 
 int Train::getOpCount() {
     return countOp;
+}
+
+void Train::addCar(bool light) {
+    Car *newCar = new Car;
+    newCar->light = light;
+
+    if (first == nullptr) {
+        first = newCar;
+        newCar->next = newCar;
+        newCar->prev = newCar;
+        return;
+    }
+
+    Car *last = first->prev;
+    newCar->next = first;
+    newCar->prev = last;
+    last->next = newCar;
+    first->prev = newCar;
+}
+
+int Train::getLength() {
+    Car *current = first;
+    current->light = true;
+
+    while (true) {
+        int steps = 0;
+
+        do {
+            current = current->next;
+            countOp++;
+            steps++;
+        } while (!current->light);
+
+        current->light = false;
+
+        for (int i = 0; i < steps; i++) {
+            current = current->prev;
+            countOp++;
+        }
+
+        if (!current->light) {
+            return steps;
+        }
+    }
 }
