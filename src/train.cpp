3d3ddf821d13b6b345506aac4a8a16ce049792a5
s4teen <112770218+s4teen@users.noreply.github.com>
2026-05-20 20:23:53 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,16 +3,6 @@
 
 Train::Train() : countOp(0), first(nullptr) {}
 
-Train::~Train() {
-    if (!first) return;
-    Car* current = first;
-    do {
-        Car* next = current->next;
-        delete current;
-        current = next;
-    } while (current != first);
-}
-
 void Train::addCar(bool light) {
     Car* newCar = new Car{light, nullptr, nullptr};
     if (!first) {
